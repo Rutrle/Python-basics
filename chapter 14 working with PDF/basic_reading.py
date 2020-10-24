4c6e0d3f86fef592cd886b27bec39650f28c6097
Jakub <rutrle@seznam.cz>
2020-10-24 15:36:45 +0200

    getting basic info about pdf

--- chapter 14 working with PDF/basic_reading.py
+++ chapter 14 working with PDF/basic_reading.py
@@ -1,2 +1,15 @@
 from PyPDF2 import PdfFileReader
 from pathlib import Path
+import os
+
+
+pdf_path = (r"chapter 14 working with PDF\Pride_and_Prejudice.pdf")
+
+
+pdf = PdfFileReader(pdf_path)
+
+
+print(pdf.getNumPages())
+print(pdf.documentInfo)  # get's the metadata
+
+print(pdf.documentInfo.title)
